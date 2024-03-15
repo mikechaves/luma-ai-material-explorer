4143922f58a980a2e77b32112b3775791bcf838d
mikejchaves <46792426+mikejchaves@users.noreply.github.com>
2024-03-15 10:40:49 -0700

    Added tailwind to index.css

---
+++ components/MaterialPreview.js
@@ -0,0 +1,4 @@
+// Create a reusable MaterialPreview component.
+// This component will represent an individual material thumbnail or preview.
+// It can display a small representation of the material (e.g., color swatch, texture).
+// Consider adding hover effects or other interactions to enhance the user experience.
