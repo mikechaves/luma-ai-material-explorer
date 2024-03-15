3ab4662e36a6975ebba68051a1c11e84322b22ab
mikejchaves <46792426+mikejchaves@users.noreply.github.com>
2024-03-15 10:31:50 -0700

    defined the project structure and added first three main components

---
+++ components/MaterialEditor.js
@@ -0,0 +1,3 @@
+// This component will serve as the main view for editing and viewing selected materials.
+// It will display the currently selected material in a large circular area.
+// Include sliders or other controls for adjusting material properties (e.g., color, roughness, reflectivity).
---
+++ components/MaterialExplorer.js
@@ -0,0 +1,3 @@
+// This component will represent the sidebar where users can browse material previews.
+// It will display a list of material thumbnails or previews.
+// Users can select a material by clicking on a preview.
